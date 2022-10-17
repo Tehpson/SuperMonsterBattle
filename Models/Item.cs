b3fb25b43c0cbfa0736bc60ccbfafe3102747179
Jesper Pettersson <jesper.pettersson@tietoevry.com>
2022-10-17 14:44:18 +0200

    added item

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,6 +1,22 @@
-﻿namespace SuperMonsterBattle.Models
+﻿using System.Collections.Generic;
+
+namespace SuperMonsterBattle.Models
 {
     public class Item
     {
+        public string Name { get; private set; }
+        public Dictionary<Stat, int> Effects { get; private set; } = new Dictionary<Stat, int>();
+
+        public Item(string Name, Dictionary<Stat, int> Effects)
+        {
+            this.Name = Name;
+            this.Effects = Effects;
+        }
+    }
+    public enum Stat
+    {
+        HP,
+        Damage,
+        Speed,
     }
 }
